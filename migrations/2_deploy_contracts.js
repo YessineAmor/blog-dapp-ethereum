00ba73a19cc9099ea1ab5787dda5afc97aaeee2c
Yassine <yassine.amor@esprit.tn>
2019-09-02 00:24:45 +0100

    update boilerplate files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var SimpleStorage = artifacts.require("./Blog.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
